646b4f6549a339cd73a5d1100b5531c036ad7941
boyi-liu <byliu0325@gmail.com>
2024-10-28 16:58:48 +0800

    reduce student into one json

--- student.py
+++ student.py
@@ -8,9 +8,11 @@ class Status(Enum):
     PINNED = 2
     ALUMNI = 3
 
-HOMEPAGE_PATH = '/Users/boy/Documents/代码/CILab'
-ALL_EN_PATH = './data/all-en.json'
-ALL_ZH_PATH = './data/all-zh.json'
+degree_list_zh = ['博士后', '博士生', '硕士生', '本科生']
+degree_list_en = ['Postdoc', 'Ph.D', 'Master', 'Undergraduate']
+
+HOMEPAGE_PATH = '/Users/boyiliu/Documents/代码/CILab'
+PEOPLE_PATH = './data/people.json'
 
 class Student:
     def generate(self):
@@ -35,18 +37,16 @@ class Student:
         item += f'<div class="cil-member-name">{name}</div>\n'
         
         if MODE == 'zh':
-            item += f'<div class="cil-member-desc">{self.degree_zh}, {self.year}</div>\n'    
+            item += f'<div class="cil-member-desc">{degree_list_zh[self.degree]}, {self.year}</div>\n'
         else:
-            item += f'<div class="cil-member-desc">{self.degree_en} {self.year}</div>\n'    
+            item += f'<div class="cil-member-desc">{degree_list_en[self.degree]} {self.year}</div>\n'
         
         if hasattr(self, 'paper'):
             item += '<div class="cil-member-honor">\n'
             item += '<img src=./static/images/icon/paper.png width="18" style="margin-right: 5px;">'
             item += '<font face="Times New Roman" size="2">'
             item += self.paper
-            item += '</font>\n'
-            item += '<br>\n'
-            item += '</div>\n'
+            item += '</font>\n<br>\n</div>\n'
         if hasattr(self, 'award_zh') or hasattr(self, 'award_en'):
             item += '<div class="cil-member-honor">\n'
             item += '<img src=./static/images/icon/award.png width="18" style="margin-right: 5px;">'
@@ -55,9 +55,7 @@ class Student:
                 item += self.award_zh
             else:
                 item += self.award_en
-            item += '</font>\n'
-            item += '<br>\n'
-            item += '</div>\n'
+            item += '</font>\n<br>\n</div>\n'
         item += '</div>\n'
         return item
     
@@ -72,9 +70,9 @@ class Student:
         item += f'<div class="cil-member-name">{name}</div>\n'
         
         if MODE == 'zh':
-            item += f'<div class="cil-member-desc">{self.degree_zh},{self.year}</div>\n'    
+            item += f'<div class="cil-member-desc">{degree_list_zh[self.degree]},{self.year}</div>\n'
         else:
-            item += f'<div class="cil-member-desc">{self.degree_en} {self.year}</div>\n'    
+            item += f'<div class="cil-member-desc">{degree_list_en[self.degree]} {self.year}</div>\n'
         
         if MODE == 'zh':
             item += f'<div class="cil-member-desc">毕业去向: <a href="{self.grad_des_link}" target="_blank" style="text-decoration: none">{self.grad_des_zh}</a> </div>\n' if hasattr(self, 'grad_des_zh') else ''
@@ -85,8 +83,8 @@ class Student:
     
 class Manager:
     def __init__(self):
-        self.people_list_zh = []
-        with open(ALL_ZH_PATH, 'r', encoding='utf-8') as file:
+        self.people_list = []
+        with open(PEOPLE_PATH, 'r', encoding='utf-8') as file:
             data = json.load(file)
             people = data.get('people')
         
@@ -94,196 +92,115 @@ class Manager:
                 s = Student()
                 for key, value in person.items():
                     setattr(s, key, value)
-                self.people_list_zh.append(s)
-                
-        self.people_list_en = []
-        with open(ALL_EN_PATH, 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            people = data.get('people')
-        
-            for person in people:
-                s = Student()
-                for key, value in person.items():
-                    setattr(s, key, value)
-                self.people_list_en.append(s)
-                
-    
-    def alumni_zh(self):
-        self.phd_list = [s for s in self.people_list_zh if s.status > 1 and (s.degree_zh == '博士生' or s.degree_zh == '博士后')]
-        self.master_list = [s for s in self.people_list_zh if s.status > 1 and s.degree_zh == '硕士生'] 
-        
+                self.people_list.append(s)
+        self.alumni_phd_list = [s for s in self.people_list if s.status > 1 and s.degree <= 1]
+        self.alumni_master_list = [s for s in self.people_list if s.status > 1 and s.degree_zh == 2]
+        self.postdoc_list = [s for s in self.people_list if s.status == 1 and s.degree == 0]
+        self.phd_list = [s for s in self.people_list if s.status == 1 and s.degree == 1]
+        self.master_list = [s for s in self.people_list if s.status == 1 and s.degree == 2]
+        self.intern_list = [s for s in self.people_list if s.status == 1 and s.degree == 3]
+
+    def alumni(self):
         item = ''
         item += '<div class="cil-container">\n'
         item += '<div class="cil-team-alumni">'
-        item += '<h1>博士生/博士后</h1>\n'
+        if MODE == 'zh':
+            item += '<h1>博士生/博士后</h1>\n'
+        else:
+            item += '<h1>Ph.D/Postdoc</h1>\n'
         item += '<div class="cil-member-list">\n'
-        
-        for phd in self.phd_list:
+
+        for phd in self.alumni_phd_list:
             item += phd.generate_alumni()
         item += '</div>\n' * 3
-        
+
         item += '<div class="cil-container">\n'
         item += '<div class="cil-team-alumni">'
-        item += '<h1>硕士生</h1>\n'
+        if MODE == 'zh':
+            item += '<h1>硕士生</h1>\n'
+        else:
+            item += '<h1>Master</h1>\n'
         item += '<div class="cil-member-list">\n'
-        
-        for m in self.master_list:
+        for m in self.alumni_master_list:
             item += m.generate_alumni()
         item += '</div>\n' * 3
-        
-        file_src = f'{HOMEPAGE_PATH}/alumni-zh.html'
+
+        if MODE == 'zh':
+            file_src = f'{HOMEPAGE_PATH}/alumni-zh.html'
+        else:
+            file_src = f'{HOMEPAGE_PATH}/alumni.html'
         start_comment = '<!-- alumni start -->'
         end_comment = '<!-- alumni end -->'
         replace(start_comment, end_comment, item, file_src)
-        
+
         # === add to index.html ===
-        self.selected_list = []
-        for m in self.master_list:
-            if m.status == 2:
-                self.selected_list.append(m)
-        for phd in self.phd_list:
-            if phd.status == 2:
-                self.selected_list.append(phd)
+        self.selected_list = [m for m in (self.alumni_master_list + self.alumni_phd_list) if m.status == 2]
+
         item = ''
         item += '<div class="cil-member-list">\n'
-        
+
         for s in self.selected_list:
             item += s.generate_alumni()
         item += '\n'
-        
-        file_src = f'{HOMEPAGE_PATH}/index-zh.html'
+
+        if MODE == 'zh':
+            file_src = f'{HOMEPAGE_PATH}/index-zh.html'
+        else:
+            file_src = f'{HOMEPAGE_PATH}/index.html'
+
         start_comment = '<!-- alumni start -->'
         end_comment = '<!-- alumni end -->'
         replace(start_comment, end_comment, item, file_src)
-        
-        
-    def alumni_en(self):
-        self.phd_list = [s for s in self.people_list_en if s.status > 1 and (s.degree_en == 'Ph.D.' or s.degree_en == 'Postdoc.')]
-        self.master_list = [s for s in self.people_list_en if s.status > 1 and s.degree_en == 'Master.'] 
-        
-        item = ''
-        item += '<div class="cil-container">\n'
-        item += '<div class="cil-team-alumni">'
-        item += '<h1>Ph.D/Postdoc</h1>\n'
-        item += '<div class="cil-member-list">\n'
-        
-        for phd in self.phd_list:
-            item += phd.generate_alumni()
-        item += '</div>\n' * 3
-        
-        item += '<div class="cil-container">\n'
-        item += '<div class="cil-team-alumni">'
-        item += '<h1>Master</h1>\n'
-        item += '<div class="cil-member-list">\n'
-        
-        for m in self.master_list:
-            item += m.generate_alumni()
-        item += '</div>\n' * 3
-        
-        
-        file_src = f'{HOMEPAGE_PATH}/alumni.html'
-        start_comment = '<!-- alumni start -->'
-        end_comment = '<!-- alumni end -->'
-        replace(start_comment, end_comment, item, file_src)
-        
-        # === add to index.html ===
-        self.selected_list = []
-        for m in self.master_list:
-            if m.status == 2:
-                self.selected_list.append(m)
-        for phd in self.phd_list:
-            if phd.status == 2:
-                self.selected_list.append(phd)
-        item = ''
-        item += '<div class="cil-member-list">\n'
-        
-        for s in self.selected_list:
-            item += s.generate_alumni()
-        item += '\n'
-        
-        file_src = f'{HOMEPAGE_PATH}/index.html'
-        start_comment = '<!-- alumni start -->'
-        end_comment = '<!-- alumni end -->'
-        replace(start_comment, end_comment, item, file_src)
+
     
-    def student_en(self):
-        self.postdoc_list = [s for s in self.people_list_en if s.status == 1 and s.degree_en == 'Postdoc.']
-        self.phd_list = [s for s in self.people_list_en if s.status == 1 and s.degree_en == 'Ph.D.']
-        self.master_list = [s for s in self.people_list_en if s.status == 1 and s.degree_en == 'Master.']
-        self.intern_list = [s for s in self.people_list_en if s.status == 1 and s.degree_en == 'Undergraduate.']
-        
+    def student(self):
         item = ''
         item += '<div class="cil-team-student">\n'
 
-        item += '<h1>Postdoc Students</h1>'
+        if MODE == 'en':
+            item += '<h1>Postdoc Students</h1>'
+        else:
+            item += '<h1>博士后</h1>'
+
         item += '<div class="cil-member-list">\n'
         for postdoc in self.postdoc_list:
             item += postdoc.generate()
         item += '</div>\n'
 
-        item += '<h1>Ph.D Students</h1>'
+        if MODE == 'en':
+            item += '<h1>Ph.D Students</h1>'
+        else:
+            item += '<h1>博士生</h1>'
+
         item += '<div class="cil-member-list">\n'
         for phd in self.phd_list:
             item += phd.generate()
         item += '</div>\n'
-        
-        item += '<h1>Master Students</h1>'
+
+        if MODE == 'en':
+            item += '<h1>Master Students</h1>'
+        else:
+            item += '<h1>硕士生</h1>'
         item += '<div class="cil-member-list">\n'
         for m in self.master_list:
             item += m.generate()
         item += '</div>\n'
 
-        item += '<h1>Undergraduate Interns</h1>'
+        if MODE == 'en':
+            item += '<h1>Undergraduate Interns</h1>'
+        else:
+            item += '<h1>本科生</h1>'
         item += '<div class="cil-member-list">\n'
         for m in self.intern_list:
             item += m.generate()
         item += '</div>\n'
         
         item += '</div>\n'
-        
-        file_src = f'{HOMEPAGE_PATH}/index.html'
-        start_comment = '<!-- student start -->'
-        end_comment = '<!-- student end -->'
-        replace(start_comment, end_comment, item, file_src)
-        
-    def student_zh(self):
-        self.postdoc_list = [s for s in self.people_list_zh if s.status == 1 and s.degree_zh == '博士后']
-        self.phd_list = [s for s in self.people_list_zh if s.status == 1 and s.degree_zh == '博士生']
-        self.master_list = [s for s in self.people_list_zh if s.status == 1 and s.degree_zh == '硕士生']
-        self.intern_list = [s for s in self.people_list_zh if s.status == 1 and s.degree_zh == '本科生']
-        
-        item = ''
-        item += '<div class="cil-team-student">\n'
 
-        item += '<h1>博士后</h1>'
-        item += '<div class="cil-member-list">\n'
-        for postdoc in self.postdoc_list:
-            item += postdoc.generate()
-        item += '</div>\n'
-
-        item += '<h1>博士生</h1>'
-        item += '<div class="cil-member-list">\n'
-        for phd in self.phd_list:
-            item += phd.generate()
-        item += '</div>\n'
-        
-        item += '<h1>硕士生</h1>'
-        item += '<div class="cil-member-list">\n'
-        for m in self.master_list:
-            item += m.generate()
-        item += '</div>\n'
-
-        item += '<h1>本科生</h1>'
-        item += '<div class="cil-member-list">\n'
-        for m in self.intern_list:
-            item += m.generate()
-        item += '</div>\n'
-
-
-        
-        item += '</div>\n'
-        
-        file_src = f'{HOMEPAGE_PATH}/index-zh.html'
+        if MODE == 'en':
+            file_src = f'{HOMEPAGE_PATH}/index.html'
+        else:
+            file_src = f'{HOMEPAGE_PATH}/index-zh.html'
         start_comment = '<!-- student start -->'
         end_comment = '<!-- student end -->'
         replace(start_comment, end_comment, item, file_src)
@@ -306,8 +223,9 @@ if __name__ == "__main__":
     m = Manager()
 
     MODE='zh'
-    m.alumni_zh()
-    m.student_zh()
+    m.alumni()
+    m.student()
+
     MODE='en'
-    m.alumni_en()
-    m.student_en()
+    m.alumni()
+    m.student()
